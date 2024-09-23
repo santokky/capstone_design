7a05f187280b5707f4499bd37c2c8873162cc13e
geonsangi <tkd1007@hs.ac.kr>
2024-09-23 23:07:35 +0900

    1
    1

--- geonsang/quicklendar/lib/screens/ocr_screen.dart
+++ geonsang/quicklendar/lib/screens/ocr_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import "package:camera/camera.dart";
+import 'package:image_picker/image_picker.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 class OCRScreen extends StatefulWidget {
   const OCRScreen({super.key});
@@ -11,117 +11,107 @@ class OCRScreen extends StatefulWidget {
 }
 
 class OCRScreenState extends State<OCRScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-  late List<CameraDescription> cameras;
-  late CameraDescription firstCamera;
+  XFile? _image; // 이미지를 저장할 변수
+  final ImagePicker picker = ImagePicker(); // ImagePicker 초기화
+  String scannedText = ""; // textRecognizer로 인식된 텍스트를 담을 String
 
-  @override
-  void initState() {
-    super.initState();
-    initializeCamera();
+  // 이미지 선택 함수 (갤러리 or 카메라)
+  Future<void> getImage(ImageSource imageSource) async {
+    final XFile? pickedFile = await picker.pickImage(source: imageSource);
+    if (pickedFile != null) {
+      setState(() {
+        _image = XFile(pickedFile.path); // 가져온 이미지를 _image에 저장
+      });
+      getRecognizedText(_image!); // 이미지를 가져온 뒤 텍스트 인식 실행
+    }
   }
 
-  Future<void> initializeCamera() async {
-    cameras = await availableCameras();
-    firstCamera = cameras.first;
-    _controller = CameraController(
-      firstCamera,
-      ResolutionPreset.medium,
-    );
-    _initializeControllerFuture = _controller.initialize();
-    setState(() {}); // 초기화된 컨트롤러를 반영하기 위해 위젯을 다시 빌드
-  }
+  // 텍스트 인식 함수
+  void getRecognizedText(XFile image) async {
+    final InputImage inputImage = InputImage.fromFilePath(image.path);
+    final textRecognizer =
+    GoogleMlKit.vision.textRecognizer(script: TextRecognitionScript.latin);
+    RecognizedText recognizedText =
+    await textRecognizer.processImage(inputImage);
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+
+    await textRecognizer.close();
+
+    scannedText = "";
+    for (TextBlock block in recognizedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = scannedText + line.text + "\n";
+      }
+    }
+
+    setState(() {}); // 상태를 갱신해 화면에 표시
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        foregroundColor: Colors.white,
-        elevation: 0,
-        title: Text('퀵린더'),
-        centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: null,),
-        actions: [
-          //IconButton(icon: Icon(Icons.search), onPressed: null,),
-          IconButton(icon: Icon(Icons.notifications), onPressed: null,),
-        ],
-      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          FutureBuilder<void>(
-            future: _initializeControllerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                // 미리보기
-                return Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(0.0), // 원하는 패딩 값 설정
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Color(0xFF33FF33), width: 3.0), // 초록색 경계선 설정
-                    ),
-                    child: CameraPreview(_controller),
-                  ),
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
+          SizedBox(height: 30, width: double.infinity),
+          _buildPhotoArea(),
+          _buildRecognizedText(),
+          SizedBox(height: 20),
+          _buildButton(),
         ],
       ),
-      // 버튼 누를 시 카메라 화면의 캡쳐본을 보여주는 화면으로 이동
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            // 현재 카메라 화면 캡쳐
-            final image = await _controller.takePicture();
-
-            if (!mounted) return;
-
-            // 사진 보여주기
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  imagePath: image.path,
-                ),
-              ),
-            );
-          } catch (e) {
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-}
 
-// --------------------------------
-// 찍은 사진 보여주는 위젯
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
+  // 갤러리에서 선택한 사진을 표시하는 영역
+  Widget _buildPhotoArea() {
+    return _image != null
+        ? Container(
+      width: 320,
+      height: 400,
+      child: Image.file(File(_image!.path)),
+    )
+        : Container(
+      width: 320,
+      height: 400,
+      color: Colors.grey,
+    );
+  }
 
-  const DisplayPictureScreen({super.key, required this.imagePath});
+  // 인식된 텍스트를 화면에 표시하는 영역
+  Widget _buildRecognizedText() {
+    return Text(
+      scannedText.isNotEmpty ? scannedText : "인식된 텍스트가 없습니다.",
+      textAlign: TextAlign.center,
+    );
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('캡쳐 화면')),
-      body: Center(
-        child: kIsWeb
-            ? Image.network(imagePath) // 웹에서는 네트워크 이미지를 사용
-            : Image.file(File(imagePath)), // 모바일에서는 파일 이미지를 사용
-      ),
+  // 버튼 빌드 함수 (카메라, 갤러리)
+  Widget _buildButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton.icon(
+          onPressed: () {
+            getImage(ImageSource.camera); // 카메라로 이미지 가져오기
+          },
+          icon: Icon(Icons.camera_alt),
+          label: Text("카메라"),
+        ),
+        SizedBox(width: 30),
+        ElevatedButton(
+          onPressed: () {
+            getImage(ImageSource.gallery); // 갤러리에서 이미지 가져오기
+          },
+          child: Row(
+            children: [
+              Icon(Icons.photo_library),
+              SizedBox(width: 5),
+              Text("갤러리"),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
